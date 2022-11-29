9804aa04f48aeaf5b097f9c0b73da7e5a929b140
Vitor <vitor.qcmendonca@senacsp.edu.br>
2022-11-29 17:23:28 -0300

    criando aplicação

---
+++ PrimeiraApp/frmLogin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiraApp
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+          MessageBox.Show("Skal seja bem vindo a aventura.", "Mensagem do sistema", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+        
+    }
+}
